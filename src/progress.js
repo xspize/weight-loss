5e28e37f0c72bc8dd737a85ead8e1f17b4035261
João Soares <majinwalt1@gmail.com>
2023-06-02 09:22:36 +0100

    mobile adapted

--- src/progress.js
+++ src/progress.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
+import './App.css';
 import CircularProgress, {
   circularProgressClasses,
 } from '@mui/material/CircularProgress';
@@ -8,7 +9,7 @@ import LinearProgress, { linearProgressClasses } from '@mui/material/LinearProgr
 
 const BorderLinearProgress = styled(LinearProgress)(({ theme }) => ({
   height: 15,
-  width: 500,
+  width: 300,
   borderRadius: 5,
   [`&.${linearProgressClasses.colorPrimary}`]: {
     backgroundColor: theme.palette.mode === 'dark' ? 'fffff' : '#ffffff',
